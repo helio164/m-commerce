e2678f7b82feb745853b6e3c1f3f3ee1e007d954
helio164 <heliocmartins@gmail.com>
2019-08-21 07:52:46 +0100

    product details update

--- mobile_shop/lib/Pages/product_details.dart
+++ mobile_shop/lib/Pages/product_details.dart
@@ -221,7 +221,7 @@ class _ProductDetailsState extends State<ProductDetails> {
           Divider(),
           new ListTile(
             title: new Text("Product details"),
-            subtitle: new Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+            subtitle: new Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
           ),
           new Row(
             children: <Widget>[
